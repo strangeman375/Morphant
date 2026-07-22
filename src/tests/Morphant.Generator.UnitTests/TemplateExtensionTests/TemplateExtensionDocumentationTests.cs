ba937c59750afee9e881b69b4595cc8b4b319814
Ухов Андрей Сергеевич <ukhov@tochka.com>
2026-07-22 16:32:53 +0700

    move single-file category

--- src/tests/Morphant.Generator.UnitTests/TemplateExtensionTests/TemplateExtensionDocumentationTests.cs
+++ src/tests/Morphant.Generator.UnitTests/TemplateExtensionTests/TemplateExtensionDocumentationTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.CodeAnalysis.CSharp;
 using Morphant.Generator.UnitTests.TestUtils;
 
-namespace Morphant.Generator.UnitTests.TemplateExtensionTests.Documentation;
+namespace Morphant.Generator.UnitTests.TemplateExtensionTests;
 
 [TestFixture]
 internal sealed class TemplateExtensionDocumentationTests
